8cb28b315de4fd3ba4f02ee11aa8314c272f13f0
Ayush Rathi <ayush1234rathi@gmail.com>
2025-07-06 17:33:30 +0530

    MongoDB Atlas connection done.

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -3,17 +3,23 @@ import dotenv from "dotenv";
 import { DB_NAME } from "../constants.js";
 
 dotenv.config({
-  path: "./.env",
+  path: "../.env",
 });
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
+    }
+
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log("MONGODB CONNECTION SUCCESSFULL !!!");
+    
+    console.log(`MONGODB CONNECTION SUCCESSFUL! DB HOST: ${connectionInstance.connection.host}`);
+    return connectionInstance;
   } catch (error) {
-    console.error("MONGODB CONNECTION ERROR :::", error);
+    console.error("MONGODB CONNECTION ERROR:", error);
     process.exit(1);
   }
 };
